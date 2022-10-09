270e9fa77bfc82c4491455ac170f15dbed00e3ec
khneide <kenan.hneide@gmail.com>
2022-10-09 02:06:45 -0700

    chore: add appsync env vars

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -3,6 +3,9 @@ const awsmobile = {
   aws_cognito_region: process.env.REACT_APP_STAGE,
   aws_user_pools_id: process.env.REACT_APP_USER_POOL_ID,
   aws_user_pools_web_client_id: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+  aws_appsync_graphqlEndpoint: process.env.REACT_APP_API_URL,
+  aws_appsync_region: process.env.REACT_APP_STAGE,
+  aws_appsync_authenticationType: process.env.REACT_APP_API_AUTH_TYPE,
   oauth: {
     redirectSignIn: process.env.REACT_APP_APP_DOMAIN,
     redirectSignOut: process.env.REACT_APP_APP_DOMAIN,
